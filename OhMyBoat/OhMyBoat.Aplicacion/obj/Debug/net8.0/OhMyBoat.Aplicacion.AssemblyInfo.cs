6cbf52c826b479d53f54d89f3b5f1352d5207213
Manuel Barreto <manubarretoamaya@gmail.com>
2024-05-01 16:17:02 -0300

    Prueba 2

--- OhMyBoat/OhMyBoat.Aplicacion/obj/Debug/net8.0/OhMyBoat.Aplicacion.AssemblyInfo.cs
+++ OhMyBoat/OhMyBoat.Aplicacion/obj/Debug/net8.0/OhMyBoat.Aplicacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OhMyBoat.Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d14a4663040cdca0cacd2346201d0abba3a1215")]
 [assembly: System.Reflection.AssemblyProductAttribute("OhMyBoat.Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OhMyBoat.Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

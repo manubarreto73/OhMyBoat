d76b88385c155ed48a2f1f5f35fc66d41fef5ab8
Manuel Barreto <manubarretoamaya@gmail.com>
2024-05-01 20:45:00 -0300

    Complete algo de la clase User

--- OMB/OMB.Aplication/ClasesBase/User.cs
+++ OMB/OMB.Aplication/ClasesBase/User.cs
@@ -1,6 +1,15 @@
-namespace OMB.Aplication.User
+namespace OMB.Aplication.User;
+
+using System;
 
 public class User {
 
+    public integer id {get; set;}
+    public string name {get; set;}
+    public string surname {get; set;}
+    public string userName {get; set;}
+    public string mail {get; set;}
+    public string number {get; set;}
+    public DateTime birthDate {get; set;}
 
 }

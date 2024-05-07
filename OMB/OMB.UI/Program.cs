71b6b600f228f72ae1cd6c27cfef21814dff8f92
Manuel Barreto <manubarretoamaya@gmail.com>
2024-05-07 20:06:27 -0300

    Debugging de Login, cartel para confirmar Logout

--- OMB/OMB.UI/Program.cs
+++ OMB/OMB.UI/Program.cs
@@ -21,26 +21,11 @@ Testing.Initialize();
 
 // ACA ESTA LO QUE AGREGAMOS NOSOTROS
 
-//ESTO ES PARA AGREGAR EL SERVICIO DE MIDDLEWARE DE SESIÓN
-
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddSession(options =>
-{
-    options.Cookie.Name = ".OMB.Session"; // Cambia el nombre de la cookie según tus preferencias
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-    options.IdleTimeout = TimeSpan.FromMinutes(30); // Establece el tiempo de expiración de la sesión
-});
-
-
 builder.Services.AddTransient<addUserUseCase>();
 builder.Services.AddTransient<deleteUserUseCase>();
 builder.Services.AddTransient<modifyUserUseCase>();
 builder.Services.AddTransient<userListUseCase>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-
-//si no agregas todos los scoped aunque no los uses no funca
 
 builder.Services.AddTransient<addVehicleUseCase>();
 builder.Services.AddTransient<deleteVehicleUseCase>();
@@ -77,12 +62,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-
-//ESTO ES PARA HABILITAR EL MIDDLEWARE DE SESIÓN
-app.UseSession();
-
-
 
 app.UseHttpsRedirection();
 

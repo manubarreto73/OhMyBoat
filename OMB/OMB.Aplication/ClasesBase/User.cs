c5607a53d42a30063c3dddc864e6c90cd4687c4c
Manuel Barreto <manubarretoamaya@gmail.com>
2024-05-08 12:14:28 -0300

    Cambios en las clase base, creación de un nuevo testing para la BD, cambios en muchos archivos

--- OMB/OMB.Aplication/ClasesBase/User.cs
+++ OMB/OMB.Aplication/ClasesBase/User.cs
@@ -1,13 +1,28 @@
 namespace OMB.Aplication.ClasesBase;
 
-public class User : Employee{
+public class User{
 
+    public int Id {get; set;}
+    public string name {get; set;}
+    public string surname {get; set;}
+    public string userName {get; set;}
+    public string password {get; set;}
+    public string mail {get; set;}
+    public string number {get; set;}
+    public DateOnly birthDate {get; set;}
     public bool banned {get; set;} = false;
     public List<Vehicle>? Vehicles {get; set;}
     public List<Ship>? Ships {get; set;}
 
-    public User(string name, string surname, string userName, string password, string mail, string number, DateOnly birthDate) : base(name, surname, userName, password, mail, number, birthDate) {
-    
+    public User(string name, string surname, string userName, string password, string mail, string number, DateOnly birthDate) {
+        this.name = name;
+        this.surname = surname;
+        this.userName = userName;
+        this.password = password;
+        this.mail = mail;
+        this.number = number;
+        this.birthDate = birthDate;
+        this.banned = false;
     }
 
     public User(){
--- OMB/OMB.UI/Testing.cs
+++ OMB/OMB.UI/Testing.cs
@@ -9,17 +9,27 @@ public class Testing{
         DateOnly birthDate = DateOnly.FromDateTime(DateTime.Today.AddYears(-21));
         using(OMBContext context = new OMBContext()){
         if(context.Users.Count() == 0){
-            context.Add(new User("Manu", "Barreto", "manubarreto", "manuel123", "manubarreto@gmail.com", "221123123", birthDate)); //1
-            context.Add(new User("Luis", "González", "luisgonzalez", "luiss123", "luisgonzalez@gmail.com", "221456456", birthDate)); //2
-            context.Add(new User("María", "Rodríguez", "mariarodriguez", "maria123", "mariarodriguez@gmail.com", "221789789", birthDate)); //3
-            context.Add(new User("Pedro", "Martínez", "pedromartinez", "pedro123", "pedromartinez@gmail.com", "221987654", birthDate)); //4
-            context.Add(new User("Ana", "Sánchez", "anasanchez", "anasan123", "anasanchez@gmail.com", "221654321", birthDate)); //5
-            context.Add(new User("Lucía", "Fernández", "luciafernandez", "lucia123", "luciafernandez@gmail.com", "221111222", birthDate)); //6
-            context.Add(new User("Javier", "Díaz", "javierdiaz", "javier123", "javierdiaz@gmail.com", "221333444", birthDate)); //7
-            context.Add(new User("Laura", "López", "lauralopez", "laura123", "lauralopez@gmail.com", "221555666", birthDate)); //8
+            context.Users.Add(new User("Manu", "Barreto", "manubarreto", "manuel123", "manubarreto@gmail.com", "221123123", birthDate)); //1
+            context.Users.Add(new User("Luis", "González", "luisgonzalez", "luiss123", "luisgonzalez@gmail.com", "221456456", birthDate)); //2
+            context.Users.Add(new User("María", "Rodríguez", "mariarodriguez", "maria123", "mariarodriguez@gmail.com", "221789789", birthDate)); //3
+            context.Users.Add(new User("Pedro", "Martínez", "pedromartinez", "pedro123", "pedromartinez@gmail.com", "221987654", birthDate)); //4
+            context.Users.Add(new User("Ana", "Sánchez", "anasanchez", "anasan123", "anasanchez@gmail.com", "221654321", birthDate)); //5
+            context.Users.Add(new User("Lucía", "Fernández", "luciafernandez", "lucia123", "luciafernandez@gmail.com", "221111222", birthDate)); //6
+            context.Users.Add(new User("Javier", "Díaz", "javierdiaz", "javier123", "javierdiaz@gmail.com", "221333444", birthDate)); //7
+            context.Users.Add(new User("Laura", "López", "lauralopez", "laura123", "lauralopez@gmail.com", "221555666", birthDate)); //8
         }
         context.SaveChanges();
         }
+        using(OMBContext context = new OMBContext()){
+        if(context.Employees.Count() == 0){
+            context.Employees.Add(new Employee("Daniel", "Perez", "jefaso", "daniel123", "danieljefaso@gmail.com", "221512748", birthDate)); //1 ADMIN
+            context.Employees.Add(new Employee("Anakin", "Skywalker", "darthvader", "padme123", "anakin@gmail.com", "221512465", birthDate)); //2
+            context.Employees.Add(new Employee("Renata", "Vargas", "renatavargas", "renata123", "renatavargas@gmail.com", "221777888", birthDate)); //3
+            context.Employees.Add(new Employee("Matías", "Fernández", "matiasfernandez", "matias123", "matiasfernandez@gmail.com", "221999000", birthDate)); //4
+        }
+        context.SaveChanges();
+        }
+
         using(OMBContext context = new OMBContext()){
         if(context.Vehicles.Count() == 0){
             context.Add(new Vehicle(3, "Auto", "ABC123", "En buen estado", "Fiat", 90000, 3));

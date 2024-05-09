01176e8b80661b8543b2723ea5bc276ce84db092
LavOwl <ide088re@gmail.com>
2024-05-09 18:04:16 -0300

    Solución fea a polimorfismo de posts

---
+++ OMB/OMB.Aplication/ClasesBase/Post.cs
@@ -0,0 +1,8 @@
+namespace OMB.Aplication.ClasesBase;
+
+public interface Post{
+
+    public bool getType();
+    public string getTitle();
+    public int getId();
+}
--- OMB/OMB.Aplication/ClasesBase/VehiclePost.cs
+++ OMB/OMB.Aplication/ClasesBase/VehiclePost.cs
@@ -1,14 +1,28 @@
+using OMB.Aplication.VehicleImageUseCases;
+
 namespace OMB.Aplication.ClasesBase;
 
-public class VehiclePost{
+public class VehiclePost : Post{
     public int Id {get; set;} //No sé si sirve de algo
     public string Title {get; set;}
     public int VehicleId {get; set;}
     public DateTime posted {get; set;}
 
+    public bool getType(){
+        return false;
+    }
+
+    public int getId(){
+        return this.VehicleId;
+    }
+
     public VehiclePost(int VehicleId, string Title){
         this.VehicleId = VehicleId;
         this.Title = Title;
         this.posted = DateTime.Now; //Creo que esto está bien??
+    }
+
+    public string getTitle(){
+        return this.Title;
     }
 }

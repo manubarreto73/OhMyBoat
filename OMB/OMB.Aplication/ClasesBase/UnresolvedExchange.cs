9eca55105f2e6574a35ba73156ad00f23d87c0d8
S-Chaves <santiagochaves242@gmail.com>
2024-05-29 15:24:25 -0300

    Listar mis intercambios

--- OMB/OMB.Aplication/ClasesBase/UnresolvedExchange.cs
+++ OMB/OMB.Aplication/ClasesBase/UnresolvedExchange.cs
@@ -1,6 +1,6 @@
 namespace OMB.Aplication.ClasesBase;
 
-public class UnresolvedExchange : Exchange{
+public class UnresolvedExchange : Exchange {
 
     public UnresolvedExchange (int transportePosteadoId, int transporteOfertadoId) : base(transporteOfertadoId, transportePosteadoId) {
     }

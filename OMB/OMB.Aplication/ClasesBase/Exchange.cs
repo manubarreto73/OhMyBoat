4e61465ed1d2c68ba665c807bd984e13d43e0b76
S-Chaves <santiagochaves242@gmail.com>
2024-05-31 23:20:05 -0300

    Agrego sede a proponer horario

--- OMB/OMB.Aplication/ClasesBase/Exchange.cs
+++ OMB/OMB.Aplication/ClasesBase/Exchange.cs
@@ -8,12 +8,13 @@ public abstract class Exchange : IClonable{
     public Transport transportePosteado {get; set;}
     public Transport transporteOfertado {get; set;}
     public DateTime fechaYHora {get; set;}
-    public string? sede {get; set;}
+    public string? sede {get; set;} // Puede estar en Sede 1 a 4
 
     public Exchange (int transportePosteadoId, int transporteOfertadoId) {
         this.transporteOfertadoId = transporteOfertadoId;
         this.transportePosteadoId = transportePosteadoId;
-        this.sede = "";
+        this.sede = "Sede 1";
+        this.fechaYHora = DateTime.Now.Date;
     }
 
     public abstract IClonable Clone();

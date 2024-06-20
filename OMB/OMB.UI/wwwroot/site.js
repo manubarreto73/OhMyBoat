cc84801d7ab277a7829dcaf8a52c6c734274872f
Manuel Barreto <manubarretoamaya@gmail.com>
2024-06-20 19:49:51 -0300

    Tipos de vehiculos más intercambiados

--- OMB/OMB.UI/wwwroot/site.js
+++ OMB/OMB.UI/wwwroot/site.js
@@ -54,17 +54,47 @@ window.renderPlotlyPieChart = function (id, values, labels, title, xSize, ySize)
     Plotly.newPlot(id, data, layout, config);
 };
 
-window.exportPlotlyChartToPdf = function (chartId1, chartId2) {
+window.renderPlotlyHorizontalBarChart = function (id, xData, yData, title, xSize, ySize) {
+    var data = [{
+        x: xData,
+        y: yData,
+        type: "bar",
+        orientation: "h",
+        marker: {
+            color: '#03045e'  // Orange color
+        }
+    }];
+
+    var layout = {
+        height: xSize,
+        width: ySize,
+        config: {
+            displayModeBar: false,
+            displaylogo: false
+        }
+    };
+
+    var config = {
+        displayModeBar: false // This will hide the mode bar
+    }
+
+    Plotly.newPlot(id, data, layout, config);
+};
+
+window.exportPlotlyChartToPdf = function (fecha) {
     const { jsPDF } = window.jspdf;
 
     const promise1 = Plotly.toImage('intercambios', { format: 'png', width: 650, height: 700*0.65, scale:3 });
-    const promise2 = Plotly.toImage('sedes', { format: 'png', width: 650, height: 700*0.65, scale:3   });
+    const promise2 = Plotly.toImage('sedes', { format: 'png', width: 650, height: 700 * 0.65, scale: 3 });
+    const promise3 = Plotly.toImage('tipos', { format: 'png', width: 650, height: 700 * 0.65, scale: 3 });
 
-    Promise.all([promise1, promise2])
-        .then(function ([imgData1, imgData2]) {
+    Promise.all([promise1, promise2, promise3])
+        .then(function ([imgData1, imgData2, imgData3]) {
             const pdf = new jsPDF();
             pdf.addImage(imgData1, 'PNG', 52, 20, 120, 75);
             pdf.addImage(imgData2, 'PNG', 19, 100, 170, 100);
-            pdf.save('charts.pdf');
+            pdf.addPage();
+            pdf.addImage(imgData3, 'PNG', 52, 20, 120, 75);
+            pdf.save('OhMyBoat-estadísticas' + fecha + '.pdf');
         });
 };
